d7899052b3cb3bbefcdef722cc37cd3b47ad2654
Muhammad Adnan Siddiqui <adsiddiquip@gmail.com>
2026-05-03 15:50:59 +0530

    changes

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ import cors from 'cors';
 import connectDB from './src/config/db.js' 
 dotenv.config();
 
-
 connectDB();
 
 
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import postRoutes from './routes/post.routes.js';
 const app = express();
 
 
-
 app.use(express.json());
 app.use('/api/auth',authRoutes);
 app.use('/api/posts',postRoutes);
